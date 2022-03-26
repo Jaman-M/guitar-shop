7c775f4f26065f9be155396d4d766f4d197d780b
Jaman-M <jaman.manik1@gmail.comm>
2022-03-26 22:06:26 +0600

    add eventhandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+    // const{handleAddToCart}=props;
     const{name, img, seller, price, ratings} = props.product;
+    
     return (
         <div className='product'>
             <img src={img} alt=''></img>
+            <div className='product-info'>
             <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Ratings: {ratings}</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add to cart</p>
+            </button>
         </div>
+        
     );
 };
 
