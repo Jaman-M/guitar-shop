c5a8f30c73c80d2b2b5d12b2cfa21e93704794e3
Jaman-M <jaman.manik1@gmail.comm>
2022-03-26 21:21:29 +0600

    added fake data ,img,name etc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <nav className='header'>
+            <h2>Guitar Shop</h2>
+            <div>
+            <a href="">best guiter</a> <a href="">get discount</a> 
+            </div>
+        </nav>
     );
 };
 
